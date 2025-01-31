43d7a9eb9b57a668f9ec7ae4a6489623cf7e3c24
Jeromy <jeromytann@gmail.com>
2025-01-31 18:04:45 +0800

    Added 3 helper functions and unit tests:
    Find blog with the most likes (favoriteBlog)
    Find author with most blogs (mostBlogs)
    Find author with most likes (mostLikes)

--- tests/likes.test.js
+++ tests/likes.test.js
@@ -2,7 +2,7 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-const blogs = [
+const manyBlogs = [
   {
     _id: "5a422a851b54a676234d17f7",
     title: "React patterns",
@@ -68,5 +68,110 @@ describe('total likes', () => {
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     assert.strictEqual(result, 5)
+  })
+
+  test('a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(manyBlogs)
+    assert.strictEqual(result, 36)
+  })
+})
+
+describe('favorite blog', () => {
+  test('of empty list is NaN', () => {
+    const blogs = []
+    const result = listHelper.favoriteBlog(blogs)
+    assert.strictEqual(result, NaN)
+  })
+
+  test('when list has only one blog equals that', () => {
+    const blogs = [
+      {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+      }
+    ]
+    const result = listHelper.favoriteBlog(blogs)
+    assert.strictEqual(result, blogs[0])
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.favoriteBlog(manyBlogs)
+    assert.strictEqual(result, manyBlogs[2])
+  })
+})
+
+describe('most blogs', () => {
+  test('of empty list is NaN', () => {
+    const blogs = []
+    const result = listHelper.mostBlogs(blogs)
+    assert.deepStrictEqual(result, {})
+  })
+
+  test('when list has only one blog equals that', () => {
+    const blogs = [
+      {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+      }
+    ]
+    const result = listHelper.mostBlogs(blogs)
+    const author = {
+        author: "Edsger W. Dijkstra",
+        blogs: 1
+    }
+    assert.deepStrictEqual(result, author)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostBlogs(manyBlogs)
+    const author = {
+      author: "Robert C. Martin",
+      blogs: 3
+  }
+    assert.deepStrictEqual(result, author)
+  })
+})
+
+describe('most likes', () => {
+  test('of empty list is NaN', () => {
+    const blogs = []
+    const result = listHelper.mostLikes(blogs)
+    assert.deepStrictEqual(result, {})
+  })
+
+  test('when list has only one blog equals that', () => {
+    const blogs = [
+      {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+      }
+    ]
+    const result = listHelper.mostLikes(blogs)
+    const author = {
+        author: "Edsger W. Dijkstra",
+        likes: 5
+    }
+    assert.deepStrictEqual(result, author)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostLikes(manyBlogs)
+    const author = {
+      author: "Edsger W. Dijkstra",
+      likes: 17
+    }
+    assert.deepStrictEqual(result, author)
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,68 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current, NaN);
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+      return {}
+  }
+  let authors = []
+  blogs.forEach(blog => {
+      const author = authors.find(a => a.author === blog.author)
+      if (author === undefined) {
+          const newAuthor = {
+              author: blog.author,
+              blogs: 1
+          }
+          authors.push(newAuthor)
+      } else {
+          const changedAuthor = {...author, blogs: author.blogs + 1}
+          authors = authors.map(a => a.author !== author.author ? a : changedAuthor)
+      }
+  })
+  let topAuthor = authors[0]
+  authors.forEach(author => {
+      if (author.blogs > topAuthor.blogs) {
+          topAuthor = author
+      }
+  })
+  return topAuthor
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+      return {}
+  }
+  let authors = []
+  blogs.forEach(blog => {
+      const author = authors.find(a => a.author === blog.author)
+      if (author === undefined) {
+          const newAuthor = {
+              author: blog.author,
+              likes: blog.likes
+          }
+          authors.push(newAuthor)
+      } else {
+          const changedAuthor = {...author, likes: author.likes + blog.likes}
+          authors = authors.map(a => a.author !== author.author ? a : changedAuthor)
+      }
+  })
+  let topAuthor = authors[0]
+  authors.forEach(author => {
+      if (author.likes > topAuthor.likes) {
+          topAuthor = author
+      }
+  })
+  return topAuthor
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
